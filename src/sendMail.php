fa068ca5ea53ffbfa5b9e86554afa4cd42266bc2
Arthur7436 <arthur.thai479@gmail.com>
2023-10-31 15:46:06 +1100

    fix title display

--- src/sendMail.php
+++ src/sendMail.php
@@ -1,14 +1,19 @@
 <?php
 
-$headers = "MIME-Version: 1.0" . "\r\n";
 
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+if ($_POST["message"]) {
 
-if ($_SERVER["REQUEST_METHOD"] == "get") {
-    $name = $_GET['name'];
-    $email = $_GET['email'];
-    $message = $_GET['message'];
-    mail("arthur.thai479@gmail.com", "New message from $name", $message, "From: $email", implode("\r\n", $headers));
+
+    mail(
+        "arthur.thai479@gmail.com",
+        "Here is the subject line",
+
+
+        $_POST["insert your message here"] . "From: an@email.address"
+    );
+
+
 }
+
 
 ?>
