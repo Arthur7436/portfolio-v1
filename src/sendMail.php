131a7ea49a419bdd7b7495b2a1b5a6f030fef879
arthur <arthur.thai479@gmail.com>
2023-10-31 18:50:25 +1100

    update

--- src/sendMail.php
+++ src/sendMail.php
@@ -1,13 +1,4 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
-if ($_POST["submit"] === "POST") {
-    $to = "arthur.thai479@gmail.com";
-    $subject = "Here is the sample subject line";
-    $message = $_POST["message"];
-    $headers = "From: jane@janedoe.com";
-
-    mail($to, $subject, $message, $headers);
+if ($_POST["message"]) {
+    mail("arthur.thai479@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
