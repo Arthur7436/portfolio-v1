59e8148e330660af09188da67c0154db48e0bcae
arthur <arthur.thai479@gmail.com>
2023-10-31 18:39:40 +1100

    update

--- src/sendMail.php
+++ src/sendMail.php
@@ -1,12 +1,13 @@
 <?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $to = "arthur.thai479@gmail.com";
     $subject = "Here is the sample subject line";
-    $message = $_POST["message"];
+    $message = $_SERVER["message"];
     $headers = "From: jane@janedoe.com";
 
     mail($to, $subject, $message, $headers);
 }
-
-
-?>
