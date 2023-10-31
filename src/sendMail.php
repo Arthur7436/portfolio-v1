5223da20aa22ad59b48193ad6837ef8a8d3b1928
Arthur7436 <arthur.thai479@gmail.com>
2023-10-31 15:39:18 +1100

    fix title display

--- src/sendMail.php
+++ src/sendMail.php
@@ -1,14 +1,88 @@
+// $headers = "MIME-Version: 1.0" . "\r\n";
+
+// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// if ($_SERVER["REQUEST_METHOD"] == "get") {
+// $name = $_GET['name'];
+// $email = $_GET['email'];
+// $message = $_GET['message'];
+// mail("arthur.thai479@gmail.com", "New message from $name", $message, "From: $email", implode("\r\n", $headers));
+// }
+
+
+
 <?php
+
+// Multiple recipients separated by comma
+
+$to = 'name1@example.com, name2@example.com';
+
+// Subject
+
+$subject = 'Office supplies - Reminder';
+
+// Message
+
+$message = '
+
+<html>
+
+<head>
+
+<title>Office supplies for March</title>
+
+</head>
+
+<body>
+
+<p>We need the following office supplies</p>
+
+<table>
+
+<tr>
+
+<th>Item</th><th>Quantity</th><th>Month</th><th>Department</th>
+
+</tr>
+
+<tr>
+
+<td>Notebook</td><td>10</td><td>March</td><td>Operations</td>
+
+</tr>
+
+<tr>
+
+<td>Chair</td><td>5</td><td>March</td><td>Marketing</td>
+
+</tr>
+
+</table>
+
+</body>
+
+</html>
+
+';
+
+// To send HTML emails, remember to set the Content-type header
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-if ($_SERVER["REQUEST_METHOD"] == "get") {
-    $name = $_GET['name'];
-    $email = $_GET['email'];
-    $message = $_GET['message'];
-    mail("arthur.thai479@gmail.com", "New message from $name", $message, "From: $email", implode("\r\n", $headers));
-}
+// Other additional headers
+
+$headers[] = 'To: John <john@example.com>, Mary <mary@example.com>';
+
+$headers[] = 'From: Supply Reminders <reminders@example.com>';
+
+$headers[] = 'Cc: name@example.com';
+
+$headers[] = 'Bcc: name@example.com';
+
+// Mail it
+
+mail($to, $subject, $message, implode("\r\n", $headers));
 
 ?>
