0d4470fe856dde1617e4508297238efcd25d9411
Arthur7436 <arthur.thai479@gmail.com>
2023-11-02 18:02:05 +1100

    update

--- src/sendMail.php
+++ src/sendMail.php
@@ -1,4 +1,4 @@
 <?php
 if ($_POST["message"]) {
-    mail("arthur.thai479@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+    mail("arthur.thai479@gmail.com", "Form to email message", $_POST["message"], "From: arthur.thai479@gmail.com");
 }
